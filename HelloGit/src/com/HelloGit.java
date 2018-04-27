4d95e9d128e192be4ade8f327a1a3ac52545eef3
Consor-502 <Consor-502@Consor-502-PC>
2018-04-27 14:44:16 +0900

    키보드로 이름 입력 받기 추가

--- HelloGit/src/com/HelloGit.java
+++ HelloGit/src/com/HelloGit.java
@@ -1,9 +1,15 @@
 package com;
 
+import java.util.*;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
 
 	}
 
